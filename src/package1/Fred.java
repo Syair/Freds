603a99be173c45b8e2f81f03a0cd1562845a6228
xuetianw <xuetianw@sfu.ca>
2017-01-17 11:18:35 -0800

    test2

--- src/package1/Fred.java
+++ src/package1/Fred.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  */
 public class Fred {
     public static void main(String [] args){
-        System.out.println("sssab");
+        System.out.println("sssabihihuihuihuihui");
         File sourceFile = new File("cccccc.text");
         try {
             sourceFile.createNewFile();
